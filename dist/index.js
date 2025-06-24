96d973c579daed203f3020e6e7c77a0bc6bce090
deburgerbug <vinits0224@gmail.com>
2025-06-24 17:22:29 +0530

    Added dist folder for .js Files

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//basic type defining
+/*let x: number = 1;
+console.log(x)
+*/
+function printId(id) {
+    console.log(`ID: ${id}`);
+}
+printId(101); // number
+printId("20"); // string
+let t = {
+    name: "ViNiT",
+    age: 21,
+    department: "Cloud operations"
+};
+console.log(t);
